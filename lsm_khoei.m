577524875c58594228d28004adbff8bbdf220662
Apostolos Papadimos <tolis.papadimos@gmail.com>
2025-03-20 14:10:29 +0200

    evala kapoies selides akoma

--- lsm_khoei.m
+++ lsm_khoei.m
@@ -1,5 +1,6 @@
 function[phi,grad_phi]=lsm_khoei(phi_nodes,N,gradN_global)
-phi=transpose(N)*(phi_nodes);
-grad_phi=transpose(gradN_global)*(phi_nodes);
-grad_phi=transpose(grad_phi);
+%selida 25/100
+    phi=transpose(N)*(phi_nodes);
+    grad_phi=transpose(gradN_global)*(phi_nodes);
+    grad_phi=transpose(grad_phi);
 end
--- ramp_enr.m
+++ ramp_enr.m
@@ -1,5 +1,5 @@
 function [psi,grad_psi]=ramp_enr(phi,grad_phi)
 %selida 20/103
-psi=abs(phi);
-grad_psi=sign(phi)*grad_phi;
+    psi=abs(phi);
+    grad_psi=sign(phi)*grad_phi;
 end
