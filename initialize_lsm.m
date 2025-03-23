e1c8700e69da0efcedd9efec8b1cb76d5cc804fd
Apostolos Papadimos <tolis.papadimos@gmail.com>
2025-03-23 19:02:27 +0200

    ekana kapoies akoma allages

--- initialize_lsm.m
+++ initialize_lsm.m
@@ -1,4 +1,5 @@
 function [phi_nodes]=initialize_lsm(node_coords,distance_func)
+%selida 24/100
     phi_nodes=zeros(size(node_coords,1),1);
 for i=1:size(node_coords,1)
     phi_nodes(i)=distance_func(node_coords(i,:));
