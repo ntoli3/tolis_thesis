468d9a6a8c7747d7c804a5955e1c7592c5f9402b
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2026-06-07 02:44:38 +0300

    Added a test with sign enrichment and it works correctly. Also added averaging of output at coincident vertices

--- tests/example_cantilever_xfem_opening.m
+++ tests/example_cantilever_xfem_opening.m
@@ -4,14 +4,14 @@ close all; clear; clc;
 Lx = 20.0;
 Ly = 4.0;
 t = 0.1;
-E_neg = 2E6;
-E_pos = 200;
+E_neg = 2E3;
+E_pos = 2E2;
 v = 0.3;
-P = 500;
+P = 50;
 
 % Mesh (πχ 22x6, 44x12, 66x18)
-nnx = 6; % zugos arithmos  
-nny = 2; % zugos arithmos 
+nnx = 16; % zugos arithmos  
+nny = 4; % zugos arithmos 
 model = XfemModel();
 [mesh, node_coords, element_nodes] = create_mesh_quad4(nnx, nny, Lx, Ly);
 model.setMesh(mesh, node_coords, element_nodes);
@@ -26,17 +26,17 @@ for n = 1:length(nodes_left)
 end
 
 % Loads
-[nodes_right] = find_nodes_with_x(Lx, model);
-for n = 1:length(nodes_right)
-    node_id = nodes_right(n);
-    model.addLoad(node_id, 2, - P / length(nodes_right));
-end
+node_bottom_right = find_single_node_with_xy(Lx, 0, model);
+model.addLoad(node_bottom_right, 2, -P);
+node_top_right = find_single_node_with_xy(Lx, Ly, model);
+model.addLoad(node_top_right, 2, P);
 
 % Level set
-dx = Lx / (nnx - 1);
-interface_position_x = Lx / 2;
-phi_handle = @(x, y) x - interface_position_x;
-psi_handle = @ramp_enr; % Π.χ. @ramp_enr, @sign_enr
+% interface_position_x = Lx / 2;
+% phi_handle = @(x, y) x - interface_position_x;
+interface_position_y = Ly / 2;
+phi_handle = @(x, y) y - interface_position_y;
+psi_handle = @sign_enr; % Π.χ. @ramp_enr, @sign_enr
 model.describeLevelSetAndEnrichment(phi_handle, psi_handle);
 
 % Run analysis
@@ -50,5 +50,5 @@ plotter = XfemPlotter(model);
 plotter.initialize();
 fig = figure;
 plotter.plotInitialStructure(fig);
-plotter.plotGaussPoints(fig)
-plotter.plotDeformedStructure(U, fig, 5E-3);
+%plotter.plotGaussPoints(fig)
+plotter.plotDeformedStructure(U, fig, 5E-4);
