2f70cd6547960e7f5379e4f6bc893adbf1c9cf6e
SerafeimBakalakos <serafeim.bakalakos@outlook.com>
2026-06-08 14:47:20 +0300

    experiment 1

--- src/xfem/plotting/save_my_figure.m
+++
@@ -1,8 +0,0 @@
-function [] = save_my_figure(~,~, fig, filename)
-
-[file,path] = uiputfile('*.fig','Save Figure As',filename);
-if ~isequal(file,0)
-    savefig(fig, fullfile(path,file));
-end
-
-end
